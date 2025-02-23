42664563d892a5dcbc21b9bfbe658ab4be91dede
suhaibbasha <suhaibbasha@icloud.com>
2025-02-23 12:33:24 +0100

    initialize...

---
+++ src/main/java/io/suhaibbasha/JavaDsaHubApplication.java
@@ -0,0 +1,13 @@
+package io.suhaibbasha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDsaHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDsaHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/suhaibbasha/java_dsa_hub/JavaDsaHubApplicationTests.java
@@ -0,0 +1,13 @@
+package io.suhaibbasha.java_dsa_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDsaHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
